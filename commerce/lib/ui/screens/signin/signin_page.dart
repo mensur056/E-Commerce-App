cfae053d1d797b6ce47957d88e0716e6b0cfdcd1
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-11-24 11:46:24 +0400

    etst

--- commerce/lib/ui/screens/signin/signin_page.dart
+++ commerce/lib/ui/screens/signin/signin_page.dart
@@ -1,5 +1,9 @@
+import 'package:commerce/const/padding/padding_all.dart';
+import 'package:commerce/const/paths/icon_paths.dart';
 import 'package:commerce/const/strings/signin_strings.dart';
+import 'package:commerce/const/strings/signup_strings.dart';
 import 'package:commerce/ui/widgets/appbar_back_button.dart';
+import 'package:commerce/ui/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
@@ -9,16 +13,50 @@ class SignInPage extends StatefulWidget {
   State<SignInPage> createState() => _SignInPageState();
 }
 
-class _SignInPageState extends State<SignInPage> {
+class _SignInPageState extends ChangeCheckBoxValue<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: Column(
-        children: [
-          const Text(ProjectSignInStrings.welcome),
-          _signInTexts(ProjectSignInStrings.description),
-        ],
+      body: Padding(
+        padding: ProjectPaddingAll.paddingAll20 * 2,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(ProjectSignInStrings.welcome),
+              _signInTexts(ProjectSignInStrings.description),
+              const CustomTextField(
+                  title: ProjectSignUpStrings.email,
+                  hintText: ProjectSignUpStrings.hintText1,
+                  imagePath: IconPaths.icMail),
+              const CustomTextField(
+                  title: ProjectSignUpStrings.password,
+                  hintText: ProjectSignUpStrings.hintText2,
+                  imagePath: IconPaths.icLock),
+              CheckboxListTile(
+                contentPadding: EdgeInsets.zero,
+                controlAffinity: ListTileControlAffinity.leading,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      ProjectSignInStrings.remember,
+                      style: TextStyle(color: Colors.grey, fontSize: 14),
+                    ),
+                    Text(
+                      style: TextStyle(decoration: TextDecoration.underline, color: Colors.grey, fontSize: 14),
+                      ProjectSignInStrings.forget,
+                    ),
+                  ],
+                ),
+                value: isCheck ? true : false,
+                onChanged: (value) {
+                  changeValue();
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -30,5 +68,14 @@ class _SignInPageState extends State<SignInPage> {
       leading: AppBarBackButton(onPress: () {}),
       title: const Text(ProjectSignInStrings.signIn),
     );
+  }
+}
+
+abstract class ChangeCheckBoxValue<T extends StatefulWidget> extends State<T> {
+  bool isCheck = false;
+  void changeValue() {
+    setState(() {
+      isCheck = !isCheck;
+    });
   }
 }
