d653bd327b0a6e490ce2fe32ba235593d2df760c
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-11-25 20:02:48 +0400

    test

--- commerce/lib/ui/screens/home/home_page.dart
+++ commerce/lib/ui/screens/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:commerce/const/border/border_radius.dart';
+import 'package:commerce/const/padding/padding_symmetric.dart';
 import 'package:commerce/const/paths/icon_paths.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -14,31 +16,41 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: TextField(
+        toolbarHeight: 70,
+        title: TextField(
           decoration: InputDecoration(
-            fillColor: Colors.grey,
+            contentPadding: EdgeInsets.zero,
+            filled: true,
+            fillColor: Colors.grey.shade200,
             focusColor: Colors.grey,
             hintText: 'Search product',
-            prefixIcon: SvgPicture.asset(IconPaths.icSearch),
-            border: const OutlineInputBorder(),
+            prefixIcon: SvgPicture.asset(IconPaths.icSearch, fit: BoxFit.none),
+            enabledBorder: const OutlineInputBorder(
+                borderRadius: ProjectBorder.borderRadius16, borderSide: BorderSide(color: Colors.transparent)),
           ),
         ),
         actions: [
-          InkWell(
-            onTap: () {},
-            child: CircleAvatar(
-              backgroundColor: Colors.grey.shade200,
-              child: ClipOval(
-                child: SvgPicture.asset(IconPaths.icBasket),
+          Padding(
+            padding: ProjectPaddingSymmetric.paddingHor8,
+            child: InkWell(
+              onTap: () {},
+              child: CircleAvatar(
+                backgroundColor: Colors.grey.shade200,
+                child: ClipOval(
+                  child: SvgPicture.asset(IconPaths.icBasket),
+                ),
               ),
             ),
           ),
-          InkWell(
-            onTap: () {},
-            child: CircleAvatar(
-              backgroundColor: Colors.grey.shade200,
-              child: ClipOval(
-                child: SvgPicture.asset(IconPaths.icNotification),
+          Padding(
+            padding: ProjectPaddingSymmetric.paddingHor8,
+            child: InkWell(
+              onTap: () {},
+              child: CircleAvatar(
+                backgroundColor: Colors.grey.shade200,
+                child: ClipOval(
+                  child: SvgPicture.asset(IconPaths.icNotification),
+                ),
               ),
             ),
           )
