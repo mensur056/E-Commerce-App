03d529e3ca843a4417f0699b42c5e83d25d92bd8
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-11-25 18:20:15 +0400

    test

--- commerce/lib/const/strings/signin_strings.dart
+++ commerce/lib/const/strings/signin_strings.dart
@@ -1,4 +1,4 @@
-class ProjectSignInStrings {
+mixin ProjectSignInStrings {
   static const String signIn = 'Sign In';
   static const String welcome = 'Welcome Back';
   static const String description = 'Sing in with email and password\nor continue with social media';
