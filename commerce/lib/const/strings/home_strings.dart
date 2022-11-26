a6cffaace754255ea744bf557a75895888cf1df3
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-11-26 11:45:19 +0400

    etst

---
+++ commerce/lib/const/strings/home_strings.dart
@@ -0,0 +1,4 @@
+class ProjectHomeStrings {
+  static const String cardTitle = 'A Summer Surprise';
+  static const String cashBack = 'Cashback 20%';
+}
--- commerce/lib/ui/screens/home/home_page.dart
+++ commerce/lib/ui/screens/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:commerce/const/border/border_radius.dart';
 import 'package:commerce/const/padding/padding_symmetric.dart';
 import 'package:commerce/const/paths/icon_paths.dart';
+import 'package:commerce/const/strings/home_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -15,48 +16,92 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 70,
-        title: TextField(
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.zero,
-            filled: true,
-            fillColor: Colors.grey.shade200,
-            focusColor: Colors.grey,
-            hintText: 'Search product',
-            prefixIcon: SvgPicture.asset(IconPaths.icSearch, fit: BoxFit.none),
-            enabledBorder: const OutlineInputBorder(
-                borderRadius: ProjectBorder.borderRadius16, borderSide: BorderSide(color: Colors.transparent)),
-          ),
-        ),
-        actions: [
+      appBar: _appBar(),
+      body: ListView(
+        children: [
           Padding(
-            padding: ProjectPaddingSymmetric.paddingHor8,
-            child: InkWell(
-              onTap: () {},
-              child: CircleAvatar(
-                backgroundColor: Colors.grey.shade200,
-                child: ClipOval(
-                  child: SvgPicture.asset(IconPaths.icBasket),
-                ),
+            padding: ProjectPaddingSymmetric.paddingHor20,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: ProjectBorder.borderRadius20,
+                gradient: LinearGradient(colors: [Colors.blue, Color(0xFF4A3298)]),
               ),
-            ),
-          ),
-          Padding(
-            padding: ProjectPaddingSymmetric.paddingHor8,
-            child: InkWell(
-              onTap: () {},
-              child: CircleAvatar(
-                backgroundColor: Colors.grey.shade200,
-                child: ClipOval(
-                  child: SvgPicture.asset(IconPaths.icNotification),
+              child: Padding(
+                padding: ProjectPaddingSymmetric.paddingHor20,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _sizedBox25(),
+                    const Text(ProjectHomeStrings.cardTitle, style: TextStyle(color: Colors.white)),
+                    _sizedBox10(),
+                    const Text(
+                      ProjectHomeStrings.cashBack,
+                      style: TextStyle(color: Colors.white, fontSize: 32),
+                    ),
+                    _sizedBox25()
+                  ],
                 ),
               ),
             ),
           )
         ],
       ),
-      body: ListView(),
+    );
+  }
+
+  SizedBox _sizedBox25() {
+    return const SizedBox(
+      height: 25,
+    );
+  }
+
+  SizedBox _sizedBox10() {
+    return const SizedBox(
+      height: 10,
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      toolbarHeight: 70,
+      title: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.zero,
+          filled: true,
+          fillColor: Colors.grey.shade200,
+          focusColor: Colors.grey,
+          hintText: 'Search product',
+          prefixIcon: SvgPicture.asset(IconPaths.icSearch, fit: BoxFit.none),
+          enabledBorder: const OutlineInputBorder(
+              borderRadius: ProjectBorder.borderRadius16, borderSide: BorderSide(color: Colors.transparent)),
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: ProjectPaddingSymmetric.paddingHor8,
+          child: InkWell(
+            onTap: () {},
+            child: CircleAvatar(
+              backgroundColor: Colors.grey.shade200,
+              child: ClipOval(
+                child: SvgPicture.asset(IconPaths.icBasket),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: InkWell(
+            onTap: () {},
+            child: CircleAvatar(
+              backgroundColor: Colors.grey.shade200,
+              child: ClipOval(
+                child: SvgPicture.asset(IconPaths.icNotification),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
